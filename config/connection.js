302bb95de4c54567228f8536f6c6c432a674ac79
Jiajie <jiajiehe@yahoo.com>
2022-08-18 00:03:04 -0700

    added connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,7 @@
-const { connect, connection } = require('mongoose');
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
-
-  connect(connectionString, {
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetwork', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
-module.exports = connection;
+module.exports = mongoose.connection;
